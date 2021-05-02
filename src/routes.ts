091f1a509b6558d1826f3eb934cf3be59d994790
Fabiane Dorneles <fabiane.mail.d@gmail.com>
2021-05-02 17:51:15 -0300

    feat: add models, activities, controllers and migrations

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,27 @@
-import { Router } from 'express';
+import { json, response, Router } from 'express';
+import { ActivityController } from './controller/ActivityController';
+import { CourseUnitController } from './controller/CourseUnitController';
+import { UserController } from './controller/UserController';
+
+interface UserRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const userController = new UserController();
+const activityController = new ActivityController();
+const courseUnitController = new CourseUnitController();
 
 const routes = Router();
 
-routes.get('/user', (request, response) => response.json({
-    message: "Hello World!"
-}));
+routes.get('/user', () => console.log("User route accessed"));
+routes.post('/user', userController.create);
+
+routes.get('/activity', () => console.log("Activity route accessed"));
+routes.post('/activity', activityController.create);
+
+routes.get('/courseunit', () => console.log("Course Unit route accessed"));
+routes.post('/courseunit', courseUnitController.create);
 
 export default routes;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import { User } from '../models/User';
+
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+
+  public async execute({name, email, password}: UserData): Promise < User | {} > {
+
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({email});
+
+    if(checkUserExists) {
+      throw new Error('Email address already exists!');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
